8f89989abb9463192965eb1fd67fa645421fdd66
/doggs <112615845+Mrching08@users.noreply.github.com>
2022-10-11 11:49:20 +0800

    Add files via upload

---
+++ BUTTONS/index.js
@@ -0,0 +1,12 @@
+let Dili = document.getElementById("No")
+
+function idont() {
+    console.log("button clicked")
+    Dili.textContent = "IT'S A NO FOR ME"
+}
+let OO = document.getElementById("yes")
+
+function yesido() {
+    console.log("button clicked")
+    Dili.textContent = "YES, I WILL MARRY YOU!"
+}
